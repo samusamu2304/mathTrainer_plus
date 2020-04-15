d6b311f49c9427356440fa5ee4fadc1755525b17
BoalaTheGod <46870006+BoalaTheGod@users.noreply.github.com>
2020-04-15 15:59:51 +0200

    Added firebase login

--- app/src/main/java/com/boala/mathtrainer/Menu.java
+++ app/src/main/java/com/boala/mathtrainer/Menu.java
@@ -1,27 +1,45 @@
 package com.boala.mathtrainer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.warkiz.widget.IndicatorSeekBar;
 import com.warkiz.widget.OnSeekChangeListener;
 import com.warkiz.widget.SeekParams;
 
-public class Menu extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
+
+public class Menu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     public static final String EXTRA_STRING = "operacion";
-    int time = 10;;
-    ImageButton suma, resta, multiplicacion, division;
-    IndicatorSeekBar seekTime;
-    DrawerLayout drawerLayout;
-    Toolbar toolbar;
+    private static final int RC_SIGN_IN = 69;
+    private int time = 10;;
+    private ImageButton suma, resta, multiplicacion, division;
+    private IndicatorSeekBar seekTime;
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private FirebaseAuth mAuth;
+    private NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +55,16 @@ public class Menu extends AppCompatActivity {
         multiplicacion = findViewById(R.id.multiplicacion);
         division = findViewById(R.id.division);
         seekTime = findViewById(R.id.seekTime);
+        mAuth = FirebaseAuth.getInstance();
+        navigationView = findViewById(R.id.navigation_view);
+        setNavigationViewListener();
+        if (mAuth.getCurrentUser() == null){
+            navigationView.getMenu().findItem(R.id.signOut).setVisible(false);
+            navigationView.getMenu().findItem(R.id.signIn).setVisible(true);
+        }else{
+            navigationView.getMenu().findItem(R.id.signIn).setVisible(false);
+            navigationView.getMenu().findItem(R.id.signOut).setVisible(true);
+        }
         seekTime.setOnSeekChangeListener(new OnSeekChangeListener() {
             @Override
             public void onSeeking(SeekParams seekParams) {
@@ -89,5 +117,53 @@ public class Menu extends AppCompatActivity {
 
             }
         });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+    public void signIn(){
+        List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
+        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(), RC_SIGN_IN);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.signIn:
+                if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+                    signIn();
+                }
+                break;
+            case R.id.signOut:
+                AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        navigationView.getMenu().findItem(R.id.signOut).setVisible(false);
+                        navigationView.getMenu().findItem(R.id.signIn).setVisible(true);
+                    }
+                });
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    private void setNavigationViewListener(){
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK){
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                navigationView.getMenu().findItem(R.id.signIn).setVisible(false);
+                navigationView.getMenu().findItem(R.id.signOut).setVisible(true);
+            }else{
+
+            }
+        }
     }
 }
